e4b634f8985eca564decfa3ac86669c904423ae4
Theophilus Confidence <spanzytheo@gmail.com>
2025-07-23 08:44:21 +0100

    modifications on app.ts

--- app.ts
+++ app.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://doughnation.vercel.app/"],
+    origin: ["http://localhost:3000", "https://doughnation.vercel.app"],
     credentials: true
   })
 );
